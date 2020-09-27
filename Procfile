2ce924bb6665b34166a8b1092f01d963c3751e3b
xmonader <xmonader@gmail.com>
2020-09-27 19:47:21 +0200

    update respawing

--- Procfile
+++ Procfile
@@ -1,7 +1,8 @@
 app1:
-    cmd: ping -c 2 google.com 
+    cmd: ping -c 1 google.com 
     use_shell: true
-
+    run_once: true
+    
 app2:
     cmd: ping -c 2 yahoo.com
     use_shell: true    
--- thecmd.py
+++ thecmd.py
@@ -20,24 +20,29 @@ class forepy:
         self.procfile = Procfile(filepath=procfile_path)
         self.procfile.build_deps_graph()
         self._running_procs = {}
+        self._done_procs = {}
         self._procs = {}
 
     def on_sigint(self, n, stack):
         print("got int")
         for p in self._running_procs.values():
             p.send_signal(signal.SIGINT)
+        exit()
 
     def on_sigterm(self, n, stack):
         print("got term")
 
         for p in self._running_procs.values():
             p.terminate()
+        exit()
 
     def on_sigstop(self, n, stack):
         print("got sigstop")
 
         for p in self._running_procs.values():
             p.send_signal(signal.SIGSTOP)
+        exit()
+
 
     def register_sighandlers(self):
         signal.signal(signal.SIGTERM, self.on_sigterm)
@@ -49,7 +54,7 @@ class forepy:
             print(proc_name, proc_info)
             cmd = proc_info.get('cmd')
             use_shell = proc_info.get('use_shell', False)
-            run_once = run_once=proc_info.get('run_once', False)
+            run_once  =  proc_info.get('run_once', False)
             cmd_check = proc_info.get('cmd_check', '')
             tcp_ports = proc_info.get('tcp_ports', [])
             udp_ports = proc_info.get('udp_ports', [])
@@ -77,15 +82,34 @@ class forepy:
 
     def all_done(self):
         return all(x.poll() is not None for x in self._running_procs.values())
+    
+    def process_chores(self):
+        running_procs = self._running_procs.copy()
+        for p_name, p in running_procs.items():
+            if p.poll() is  not None:
+                # it was finished
+                if self._procs[p_name].run_once:
+                    self._done_procs[p_name] = p
+                    del self._running_procs[p_name]
+                else:
+                    print(f"***respawing {p_name}..")
+                    self._running_procs[p_name] = self._procs[p_name].run()
 
-    def start_watching(self):
+    @property
+    def readers(self):
+        self.process_chores()
+
         stdoutreaders = [p.stdout for p in self._running_procs.values()]
         stderrreaders = [p.stderr for p in self._running_procs.values()]
         inputs = [*stdoutreaders, *stderrreaders]
+        return inputs
+
+    def start_watching(self):
+        inputs = self.readers
         while inputs and not self.all_done():
-            print(self._running_procs)
+            # print(self._running_procs)
             # print("all_done: ", self.all_done())
-            print([(x, x.closed, x.readable()) for x in inputs])
+            # print([(x, x.closed, x.readable()) for x in inputs])
             inputs = [x for x in inputs if not x.closed]
             # print("now inputs are: ", inputs)
             # Wait for at least one of the sockets to be
@@ -96,6 +120,7 @@ class forepy:
                 line = r.readline()
                 if line.strip():
                     print(line)
+            inputs = self.readers
 
 @click.command()
 @click.option('--procfile', type=click.Path(exists=True, readable=True), help="procfile path")
